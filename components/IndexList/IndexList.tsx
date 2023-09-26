7c2ad4af247459e5482ef231ee7b0226058bdffe
Islam <isxmad@gmail.com>
2023-09-26 13:30:52 +0500

    f

--- components/IndexList/IndexList.tsx
+++ components/IndexList/IndexList.tsx
@@ -1,14 +1,16 @@
 import styles from './IndexList.module.scss';
-
+import Link from 'next/link';
 import { FC } from 'react'
 
 const IndexList = ({list = []}: {list?: any[]}) => {
+  console.log(list)
   return (
     <div className={styles.wrapper}>
       {
         list?.length > 0 && (
           list?.map((i) => (
             <article key={i?.id} className={styles.item}>
+              <Link href={`/itm/${i?.id}`}>More</Link>
               <div className={styles.img}>
                 <img src={i?.cover_url} alt={i?.title} />
               </div>
