028e145c86c9434cd3d01043b9412763bdf927a1
Islam Madaminov <isxmad@gmail.com>
2023-10-15 04:01:10 -0700

    refactor: страница 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,15 @@
 import ContentLayout from "@/components/ContentLayout/ContentLayout"
 import PageTitle from "@/components/PageTitle/PageTitle"
+import Head from "next/head";
+
 const NotfoundPage = () => {
   return (
     <ContentLayout>
+      <Head>
+        <title>Not Found - HandmadeP</title>
+      </Head>
       <PageTitle
-        title={'404, not found('}
+        title={'404, not found :('}
         />
     </ContentLayout>
   )
